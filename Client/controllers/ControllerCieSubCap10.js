f62b1bec80c8518547c1b5c7238b69fa756076ee
xaipo <xaipoj@hotmail.com>
2017-07-25 15:06:48 -0500

    add library and controller cie10 subcategoria

---
+++ Client/controllers/ControllerCieSubCap10.js
@@ -0,0 +1,9 @@
+/**
+ * Created by xaipo on 7/25/2017.
+ */
+app.controller('ControllerNewCieCap10', ['$scope', '$http', '$location', 'myProvider', '$localStorage', function ($scope, $http, $location, myProvider, $localStorage) {
+
+
+
+
+}]);
